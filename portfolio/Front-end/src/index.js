93c3dddb6e5821e1c58003e84af786aa6a14f828
matheuszuge <matheusrzuge@gmail.com>
2024-03-10 19:11:46 -0300

    correções

---
+++ portfolio/Front-end/src/index.js
@@ -0,0 +1,9 @@
+const mapa = L.map("mapa").setView([-26.301864,-48.849932], 16)
+const layer = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}', {
+	minZoom: 0,
+	maxZoom: 13,
+	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	ext: 'png'
+});
+
+layer.addTo(mapa);
